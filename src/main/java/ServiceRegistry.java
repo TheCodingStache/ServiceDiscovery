19de39ab23d4d4b5e63df0411f45dc3f94bad438
TheCodingStache <cs4415161@teilar.gr>
2020-04-08 21:03:53 +0300

    Service Registry implementation

---
+++ src/main/java/ServiceRegistry.java
@@ -0,0 +1,10 @@
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+
+public class ServiceRegistry implements Watcher {
+    
+    @Override
+    public void process(WatchedEvent watchedEvent) {
+
+    }
+}
